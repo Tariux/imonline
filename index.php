69d016e139b5b6585a65ba9911dd8ab398bf2204
Tariux <tariux@protonmail.com>
2022-09-07 00:30:09 +0430

    a little update

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php 
+    include_once("app/lib/SDate/SDate.php");
+    $Date = new SDate;
+    
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -5,10 +10,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/fonts.css">
+
+    <link rel="shortcut icon" href="static/images/favicon.png" type="image/x-icon">
+
+    <link rel="stylesheet" href="static/css/reset.css">
+    <link rel="stylesheet" href="static/css/fonts.css">
+    <link rel="stylesheet" href="static/css/styles.css">
+
     <title>ImOnline</title>
 </head>
 <body>
     <h2>hello from my localhost GitHub!</h2>
+
+    <h1>
+        <?php
+            echo $Date->getMonthName();
+        ?>
+    </h1>
 </body>
 </html>
