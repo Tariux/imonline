20c943a139705c27895b225248569102375c8a56
Tariux <tariux@protonmail.com>
2022-09-08 02:54:52 +0430

    database functions for localhost's added! 3AM lmao

---
+++ app/feauters/database-func.php
@@ -0,0 +1,44 @@
+<?php 
+class MyDataBase {
+
+    public static function MySqlDB(
+
+    $host = "localhost" ,
+    $username = "root" ,
+    $password = "" ,
+    $db = "imonline" ,
+    $port = "3306",
+    $charset = 'utf8mb4',
+    $options = [
+        \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
+        \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+        \PDO::ATTR_EMULATE_PREPARES   => false,
+    ]
+
+    ) {
+
+        $dsn = "mysql:host=$host;dbname=$db;charset=$charset;port=$port";
+
+        try {
+
+            $pdo = new \PDO($dsn, $username, $password, $options);
+
+            if ($pdo) {
+                return $pdo;
+            }
+
+        } catch (\PDOException $e) {
+
+            throw new \PDOException($e->getMessage(), (int)$e->getCode());
+            return $e;
+
+        }
+
+
+    }
+ 
+}
+
+
+$db = MyDataBase::MySqlDB();
+print_r($db);
