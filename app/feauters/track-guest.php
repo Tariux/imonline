5e2a9d0890a2b287cdb18c3ed8b63c4691ccaf83
Tariux <tariux@protonmail.com>
2022-09-08 13:38:52 +0430

    seprateArray function added! just transform array to cool string for database-display-...

--- app/feauters/track-guest.php
+++ app/feauters/track-guest.php
@@ -4,13 +4,55 @@ require_once("./database-func.php");
 
 $db = MyDataBase::MySqlDB();
 
+function seprateArray($arr, $dataType = "index", $seprator = ",")
+{
+    if (empty($arr)) {
+        return false;
+    }
+    $sepratedStr = "";
 
+    switch ($dataType) {
+        case "value":
+
+            foreach ($arr as $str_section) {
+                $sepratedStr = $sepratedStr . '"' . $str_section . '"' . $seprator;
+            }
+
+            $sepratedStr = substr($sepratedStr, 0, -1);
+
+            break;
+
+        case "db":
+
+            foreach ($arr as $str_section) {
+                $sepratedStr = $sepratedStr . '`' . $str_section . '`' . $seprator;
+            }
+
+            $sepratedStr = substr($sepratedStr, 0, -1);
+
+            break;
+
+        default:
+
+            foreach ($arr as $str_section) {
+                $sepratedStr = $sepratedStr . $str_section . $seprator;
+            }
+
+            $sepratedStr = substr($sepratedStr, 0, -1);
+
+            break;
+    }
+
+
+
+    return $sepratedStr;
+}
 function questSessionId()
 {
     $session_id = session_id() ? sha1(session_id()) : false;
 
     if ($session_id === false) {
-        trigger_error("hey bro! please session_start() safe first!" , E_USER_NOTICE);
+        trigger_error("hey bro! please session_start() safe first!", E_USER_NOTICE);
         return false;
     }
 
@@ -21,24 +63,15 @@ function questIp()
 {
     if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
         $ip_address = $_SERVER['HTTP_CLIENT_IP'];
-    }
-
-    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))  
-    {
+    } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
         $ip_address = $_SERVER['HTTP_X_FORWARDED_FOR'];
-    }
-
-    elseif
-    (
-        isset($_SERVER['REMOTE_ADDR']) 
+    } elseif (
+        isset($_SERVER['REMOTE_ADDR'])
         && $_SERVER['REMOTE_ADDR'] != "127.0.0.1"
         && $_SERVER['REMOTE_ADDR'] != "192.168.1.1"
-    )
-    {
+    ) {
         $ip_address = $_SERVER['REMOTE_ADDR'];
-    }
-
-    else {
+    } else {
         $ip_address = "localhost";
     }
 
@@ -46,7 +79,7 @@ function questIp()
 }
 
 session_start();
-print_r(questIp());
-print_r(questSessionId());
-
+//print_r(questIp());
+//print_r(questSessionId());
+print_r(seprateArray(['mamad', 'reza', 'dude'], "db", " ,"));
 // find   Beauty   in   the   code!
